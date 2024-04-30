adb2920c54d1bd4a486221813bd972776ce3abb7
NStoyanov95 <nstoyanov9512@gmail.com>
2024-04-30 22:09:01 +0300

    Add route paths to navigation menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PublicationsComponent } from './car/publications/publications.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PublicationsComponent } from "./car/publications/publications.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: PublicationsComponent },
+  { path: "", pathMatch: "full", component: HomeComponent },
+  { path: "publications", component: PublicationsComponent },
 ];
 
 @NgModule({
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [MatToolbarModule, CommonModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+  ],
   exports: [HeaderComponent],
 })
 export class CoreModule {}
