d9464785e1d55d7e21b902eb279916f0def3dabe
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-01 11:55:22 +0300

    Add reviews component to to router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { PublicationsComponent } from "./blog/publications/publications.component";
 import { HomeComponent } from "./blog/home/home.component";
+import { ReviewsComponent } from "./blog/reviews/reviews.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
   { path: "publications", component: PublicationsComponent },
+  { path: "reviews", component: ReviewsComponent },
 ];
 
 @NgModule({
