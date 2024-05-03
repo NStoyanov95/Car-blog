f859fb64202a1f73fd5e2e6d348c26bdd3266a01
NStoyanov95 <nstoyanov9512@gmail.com>
2024-05-03 20:19:28 +0300

    Add news component to routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { PublicationsComponent } from "./blog/publications/publications.component";
 import { HomeComponent } from "./blog/home/home.component";
 import { ReviewsComponent } from "./blog/reviews/reviews.component";
+import { NewsComponent } from "./blog/news/news.component";
 
 const routes: Routes = [
   { path: "", pathMatch: "full", component: HomeComponent },
   { path: "publications", component: PublicationsComponent },
   { path: "reviews", component: ReviewsComponent },
+  { path: "news", component: NewsComponent },
 ];
 
 @NgModule({
